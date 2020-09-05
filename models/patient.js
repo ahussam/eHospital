164c0c8fe075e80a3810688110c52a87d0bf008f
Abdullah Hussam <eye.magicme@gmail.com>
2020-09-05 04:01:20 +0300

    Update patient.js

--- models/patient.js
+++ models/patient.js
@@ -1,25 +1,20 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
- patientRecord = new Schema( {
-	
-	unique_id: Number,
-	name: String,
+patientRecord = new Schema({
+
+    unique_id: Number,
+    name: String,
     case: String,
     priority: Number,
-	phoneNumber: String,
+    phoneNumber: String,
     bio: String,
-    files: String, 
-    room : Number,
+    files: String,
+    room: Number,
     assignedTo: Number
 });
 
-patientRecord.methods.toJSON = function() {
-	var obj = this.toObject();
-	delete obj.password;
-	return obj;
-   }
 
-  Patient = mongoose.model('patient', patientRecord);
+Patient = mongoose.model('patient', patientRecord);
 
 module.exports = Patient;
